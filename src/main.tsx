90518fa57c9857b1aa3dc037d1bebd3bac67fecd
absterr <enzsama.dev@gmail.com>
2026-08-24 17:42:21 +0100

    add tanstack router and import alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById("root")!).render(
+const router = createRouter({
+  routeTree,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    react(),
-    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
   ],
 });
